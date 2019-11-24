0931fcea8df62cd18bce958b7c835f9f7c74fe97
ketharee <kethare@hotmail.com>
2019-11-24 12:58:12 -0500

    Add project files.

---
+++ Basic_OOPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Basic_OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> objStudents = new List<Student>();
+
+            objStudents.Add(new Student().SetStudentInfo(101, "Arun"));
+            objStudents.Add(new Student().SetStudentInfo(102, "Bala"));
+            objStudents.Add(new Student().SetStudentInfo(103, "Chandru"));
+
+            foreach (Student student in objStudents)
+            {
+                Console.WriteLine(student.Rollnumber);
+                Console.WriteLine(student.Name);
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_OOPS/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_OOPS
+{
+    public class Student
+    {
+        #region Variables
+        private int _rollnumber;
+        private string _name;
+        #endregion
+
+        #region Properties
+        public int Rollnumber
+        {
+            get { return _rollnumber; }
+            set { _rollnumber = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        #endregion
+
+        #region Methods
+        public Student SetStudentInfo(int rollnumber, string name)
+        {
+            return new Student { Rollnumber = rollnumber, Name = name };
+        }
+        #endregion               
+
+    }
+}
